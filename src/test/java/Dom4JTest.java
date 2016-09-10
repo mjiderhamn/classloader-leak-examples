936346792bbc2c6bddc0ce41370b9ad485998964
Mattias Jiderhamn <mattias.jiderhamn@readsoft.com>
2016-09-10 15:37:29 +0200

    Removing annotations from first

--- src/test/java/Dom4JTest.java
+++ src/test/java/Dom4JTest.java
@@ -1,16 +1,11 @@
 import org.dom4j.DocumentFactory;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import se.jiderhamn.classloader.leak.JUnitClassloaderRunner;
-import se.jiderhamn.classloader.leak.Leaks;
 
 /** 
  * {@link DocumentFactory#singlePerThread} 
  */
-@RunWith(JUnitClassloaderRunner.class)
 public class Dom4JTest {
   
-  @Leaks(dumpHeapOnError = true)
   @Test
   public void dom4j() {
     
